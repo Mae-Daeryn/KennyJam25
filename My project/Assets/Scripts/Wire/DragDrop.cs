273235c3e796915a5ffb66d8c330424d92071fd9
HabEinKeks <habeinkeks@gmail.com>
2025-07-20 00:34:09 +0200

    added click sound

--- My project/Assets/Scripts/Wire/DragDrop.cs
+++ My project/Assets/Scripts/Wire/DragDrop.cs
@@ -1,16 +1,21 @@
+using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class DragDrop : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IEndDragHandler, IDragHandler
 {
 
     private RectTransform rectTransform;
     [SerializeField] private Canvas canvas;
+    private CanvasGroup canvasGroup;
+    public AudioSource audio;
     private void Awake()
     {
         rectTransform = GetComponent<RectTransform>();
+        canvasGroup = GetComponent<CanvasGroup>();
     }
     public void OnPointerDown(PointerEventData eventData)
     {
@@ -29,9 +34,30 @@ public class DragDrop : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, I
         rtNeu.anchoredPosition = rtOriginal.anchoredPosition;
         rtNeu.sizeDelta = rtOriginal.sizeDelta;
         rtNeu.localScale = rtOriginal.localScale;
+
+
+        gameObject.transform.localScale = new Vector3(1,1,1);
     }
     public void OnEndDrag(PointerEventData eventData)
     {
+        canvasGroup.blocksRaycasts = true;
 
+        List<RaycastResult> results = new List<RaycastResult>();
+        EventSystem.current.RaycastAll(eventData, results);
+
+        foreach (RaycastResult result in results)
+        {
+            GameObject target = result.gameObject;
+
+            if (target.CompareTag("empty"))
+            {
+                target.GetComponent<Image>().sprite = gameObject.GetComponent<Image>().sprite;
+                Destroy(gameObject);
+                audio.Play();
+                return;
+            }
+        }
+
+        Destroy(gameObject);
     }
 }
